bbb06e58e30643504a8ae76e2e68a5e973e60a43
Philipp Mössl <pmoessl@outlook.de>
2023-12-11 17:55:51 +0100

    add urls

--- kanban/views.py
+++ kanban/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.authtoken.views import APIView, ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+class LoginView(ObtainAuthToken):
+    pass
+
+class RegisterView():
+    pass
+
+class BoardView():
+    pass
--- kanban_backend/urls.py
+++ kanban_backend/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from kanban.views import LoginView, RegisterView , BoardView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view()),
+    path('register/', RegisterView.as_view()),
+    path('board/', BoardView.as_view()),
 ]
