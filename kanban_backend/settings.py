a0dbdc3f66571ee72aba7b089d0418815393af8c
Philipp Mössl <pmoessl@outlook.de>
2024-01-08 13:31:30 +0100

    set debug to false

--- kanban_backend/settings.py
+++ kanban_backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ux7l%!-n8fms1245)=a_pc47q-u9hsz%)n$u*+d-qy1j_-jaek'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
